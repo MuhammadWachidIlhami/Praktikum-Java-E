738e42bef1c0960a5e2781fbe3053e50dfb6da8e
MuhammadWachidIlhami <muhammadwachidilhami@gmail.com>
2021-05-23 20:10:19 +0800

    Upload Project

---
+++ PraktikumJavaE Fix/src/praktikumjavae/fix/PraktikumJavaEFix.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikumjavae.fix;
+
+/**
+ *
+ * @author USER
+ */
+public class PraktikumJavaEFix {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Halo Selamat Belajar Java");
+    }
+    
+}
